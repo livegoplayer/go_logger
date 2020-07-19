4d965eca0b613cb2dd97bb4794f287cb9ca525d4
bug <2532917800@qq.com>
2020-07-19 15:28:52 +0800

    fsadf

--- log.go
+++ log.go
@@ -9,51 +9,38 @@ var Logger *logrus.Logger
 //主动log的一些方法
 func Panic(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.PanicLevel.MarshalText()
-	Logger.Panicf("{{level,%s},{levelNo,%s},{message:%s}",
-		text, logrus.PanicLevel, message)
+	Logger.Panicf(message)
 }
 
 func Fatal(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.FatalLevel.MarshalText()
-	Logger.Fatalf("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.FatalLevel, message)
+	Logger.Fatal(message)
 }
 
 func Error(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.ErrorLevel.MarshalText()
-	Logger.Errorf("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.ErrorLevel, message)
+	Logger.Error(message)
+
 }
 
 func Warning(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.WarnLevel.MarshalText()
-	Logger.Warnf("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.WarnLevel, message)
+	Logger.Warn(message)
 }
 
 func Info(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.InfoLevel.MarshalText()
-	Logger.Infof("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.InfoLevel, message)
+	Logger.Info(message)
 }
 
 func Debug(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.DebugLevel.MarshalText()
-	Logger.Debugf("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.DebugLevel, message)
+	Logger.Debug(message)
 }
 
 func Trace(message string) {
 	Logger = GetLogger()
-	text, _ := logrus.TraceLevel.MarshalText()
-	Logger.Tracef("{{level,%s},{levelNo,%s},{message:%s},{clientIP,%15s}",
-		text, logrus.TraceLevel, message)
+	Logger.Trace(Logger)
 }
 
 func GetLogger() *logrus.Logger {
